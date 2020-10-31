eeb08231324dbacaaa05ae29ba405aed55bccf2e
german nunez <german90nunez@gmail.com>
2020-10-31 14:24:47 -0500

    configed global sass, leftnav and uppernav created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      flat
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink ml-4"
+          contain
+          src="https://chain.link/assets/images/icon-logo-white-ef878c98.svg"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title class="ml-5"> <span class="title font-weight-bold">LINK</span> News</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <SideMenu/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideMenu from './components/SideMenu';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SideMenu,
+  },
+};
+</script>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-navigation-drawer
+        mini-variant
+        mini-variant-width="100"
+        dark
+        color="primary"
+        floating
+        permanent>
+
+        <v-list flat>
+            <v-list-item>
+                <v-item-content></v-item-content>
+            </v-list-item>
+            <v-list-item-group v-model="active" color="blue darken-4">
+                <v-list-item 
+                  v-for="(navOption, index) in navOptions"
+                  :key="index">
+                    <v-list-item-content>
+                        <v-icon class="mb-2">{{navOption.icon}}</v-icon>
+                        <v-list-item-subtitle align="center">{{navOption.title}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name:'SideMenu',
+    data() {
+        return {
+            active: 0,
+            navOptions: [
+                {
+                    title: 'Dashboard', icon: 'mdi-view-dashboard'
+                },
+                {
+                    title:'Account', icon: 'mdi-account-details'
+                },
+                {
+                    title:'Reports', icon: 'mdi-file-chart'
+                },
+                {
+                    title:'Settings', icon: 'mdi-tune'
+                } 
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
